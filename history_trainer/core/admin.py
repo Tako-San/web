516b0c787a9dc3fa0e461da18fee7866aeebbe1d
Tako-San <dxe13@mail.ru>
2025-04-13 17:23:11 +0300

    Added events and questions to admin

--- history_trainer/core/admin.py
+++ history_trainer/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import HistoricalEvent, QuizQuestion
 
-# Register your models here.
+admin.site.register(HistoricalEvent)
+admin.site.register(QuizQuestion)
